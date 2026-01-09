e8d4d2783500eda7136118b09eb04426cf2f6d2c
ArgThrPro19 <166652797+ArgThrPro19@users.noreply.github.com>
2026-01-08 20:50:32 -0500

    Complete 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    if(!(Number.isInteger(a) && Number.isInteger(b))|| (a < 0 || b < 0)){
+        return `ERROR`;
+    }
+    let down, up;
+    down = (a >= b) ? b : a;
+    up = (a >= b) ? a : b;
+    let sum = 0;
+    for(let i = down; i <= up; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+// Complete
