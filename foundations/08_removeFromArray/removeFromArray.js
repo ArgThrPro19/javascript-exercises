1b4cb03867986f858d5d27db55efba37ecbe2c8b
ArgThrPro19 <166652797+ArgThrPro19@users.noreply.github.com>
2026-01-08 20:36:25 -0500

    Complete foundations 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...param) {
+    for(const rem of param){
+        for(let i = 0; i < arr.length; i++){
+            if(arr[i] === rem){
+                arr.splice(i, 1);
+                i--;
+            }
+        }   
+    }
+    return arr;
+
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+// COMPLETE
