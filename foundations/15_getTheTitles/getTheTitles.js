844e67960515871d86d87bb05ec1c2dd5dc53685
ArgThrPro19 <166652797+ArgThrPro19@users.noreply.github.com>
2026-01-14 14:58:53 -0500

    Complete foundations 15

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    returnArr = [];
+    arr.forEach(book => returnArr.push(book.title));
+    return returnArr;
 };
+
+
+//COMPELTE (shoudlhave used √√map)
 
 // Do not edit below this line
 module.exports = getTheTitles;
