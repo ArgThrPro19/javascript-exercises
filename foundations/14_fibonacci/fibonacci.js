2f5872bf973a48608ac4085d059750cd15969505
ArgThrPro19 <166652797+ArgThrPro19@users.noreply.github.com>
2026-01-14 14:54:38 -0500

    Complete foundations 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = +num;
+    if(num < 0 )
+        return "OOPS";
+    if(num === 0){
+        return 0;
+    }
+    if(num === 1){
+        return 1;
+    }
+    if(num === 2)
+        return 1;
+    
+    return fibonacci(num-1) + fibonacci(num - 2);
 };
+
+//COMPLETE
+
 
 // Do not edit below this line
 module.exports = fibonacci;
