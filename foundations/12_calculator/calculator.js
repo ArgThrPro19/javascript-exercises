e77297051eef9e12fe41b67ee3f03ffd404f3b06
ArgThrPro19 <166652797+ArgThrPro19@users.noreply.github.com>
2026-01-14 14:30:50 -0500

    Complete foundation 12

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,26 +1,40 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a , b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum = 0
+  for(const num of arr){
+    sum += num;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let sum = 1;
+  for(const num of arr){
+    sum *= num;
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base**exp; 
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+  for(let i = n; i > 0; i--){
+    result *= i;
+  }
+  return result; 
 };
+
+//COMPLETE
 
 // Do not edit below this line
 module.exports = {
