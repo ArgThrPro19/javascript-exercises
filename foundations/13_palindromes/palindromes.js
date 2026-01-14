617cdda3dfd0749d78c2365299b36678b7fa924d
ArgThrPro19 <166652797+ArgThrPro19@users.noreply.github.com>
2026-01-14 14:45:07 -0500

    Complete foundations 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,27 @@
-const palindromes = function () {
-
+const palindromes = function (inputString) {
+    const cleanString = stringCleaner(inputString.toLowerCase());
+    let reverseString = "";
+    for(let i = cleanString.length-1; i >= 0; i--){
+        reverseString += cleanString[i];
+    }
+    if(reverseString === cleanString){
+        return true;
+    }
+    return false; 
 };
+
+const stringCleaner = function(inputString){
+    let returnString = inputString;
+    const cleaner = ` ().!,:;`
+    for(let i = 0; i < cleaner.length; i++){
+        returnString = returnString.replaceAll(cleaner[i], "");
+    }
+    return returnString;
+}
+
+
+//COMPLETE 
+
 
 // Do not edit below this line
 module.exports = palindromes;
